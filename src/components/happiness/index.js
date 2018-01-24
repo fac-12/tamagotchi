1ff33eee5ab59045af6bd6c7ac0d1af04209cb64
Johanna-hub <jyherman@gmail.com>
2018-01-24 11:04:29 +0000

    functioning happy progress bar

--- src/components/happiness/index.js
+++ src/components/happiness/index.js
@@ -4,19 +4,33 @@ import "./style.scss";
 export class Sad extends React.Component {
   state = {
     feelSad: "I feel sad. Please love me.",
-    num: 0
+    num: 0,
+    progress: 0
   };
 
   incrementNum = () => {
     this.state.num === 0 ? this.setState({ feelSad: "Thank you" }) : false;
-    this.setState({ num: this.state.num + 1 });
+    this.setState({
+      num: this.state.num + 1,
+      progress: this.state.progress + 10
+    });
+    this.state.num >= 10
+      ? this.setState({
+          num: 0,
+          progress: 0,
+          feelSad: "I'm sad again. Love me again."
+        })
+      : false;
   };
 
   render() {
+    const incrementHeight = {
+      height: `${this.state.progress}%`
+    };
     return (
       <div>
         <div className="sad-bar">
-          <div className="sad-bar-progress" />
+          <div className="sad-bar-progress" style={incrementHeight} />
         </div>
         <p>{this.state.feelSad}</p>
         <p>{this.state.num}</p>
